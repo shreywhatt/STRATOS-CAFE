34273d741cf7931eadf8fb5c7efa6269f0682282
Shreya <shreyasirsale@gmail.com>
2025-06-26 23:59:39 +0530

    Food page for items displayed on the main food page.

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:stratos_cafe/utils/dimensions.dart';
 import 'package:stratos_cafe/widgets/app_icon.dart';
 
+import '../../utils/colors.dart';
+import '../../widgets/big_texts.dart';
+import '../../widgets/icon_and_text_widget.dart';
+import '../../widgets/small_texts.dart';
+
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({super.key});
 
@@ -46,6 +51,44 @@ class PopularFoodDetail extends StatelessWidget {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(Dimensions.radius20),
                   color: Colors.white,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigTexts(text: "MAHARASHTRIAN SPICE", ),
+                    SizedBox(height: Dimensions.height10,),
+                    Row(
+                      //rating stars and comments etc
+                      children: [
+                        //rating stars comments etc
+                        Wrap(
+                          children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor)),
+                        ),
+                        SizedBox(width: Dimensions.width10,),
+                        SmallTexts(text: "4.5"),
+                        SizedBox(width: Dimensions.width10,),
+                        SmallTexts(text: "1287 "),
+                        SmallTexts(text: "Comments"),
+                      ],
+                    ),
+
+                    //third row of smaller box having three more columns - location time etc
+                    SizedBox(height: Dimensions.height20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconAndTextWidget(icon: Icons.circle_sharp,
+                          text: "1.7km",
+                          iconColor: AppColors.iconColor1,),
+                        IconAndTextWidget(icon: Icons.location_on,
+                          text: "Normal",
+                          iconColor: AppColors.mainColor,),
+                        IconAndTextWidget(icon: Icons.access_time_rounded,
+                          text: "Normal",
+                          iconColor: AppColors.iconColor2,)
+                      ],
+                    )
+                  ],
                 ),
 
           )),
